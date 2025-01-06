607c955b295e5402aa4558defa0e5ad571717f1f
wolvth <73527787+wolvth@users.noreply.github.com>
2025-01-06 20:26:53 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -10,31 +10,39 @@ echo "Building for ROOTFS_PARTSIZE: $ROOTFS_PARTSIZE"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 
 
-# 定义所需安装的包列表
+# 定义所需安装的包列表 23.05.4
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-daed-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
-PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES python3"
+PACKAGES="$PACKAGES libopenssl-legacy"
+PACKAGES="$PACKAGES jq"
+PACKAGES="$PACKAGES screen"
 PACKAGES="$PACKAGES btop"
-PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
+PACKAGES="$PACKAGES -cpufreq"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-daed-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-cpufreq-zh-cn"
+PACKAGES="$PACKAGES openssh-sftp-server"
+#PACKAGES="$PACKAGES luci-i18n-arpbind-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/openwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
