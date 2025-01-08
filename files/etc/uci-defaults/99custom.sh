d9781e7d0fbd17c0f73a4c35242bcf48b47375d7
wolvth <73527787+wolvth@users.noreply.github.com>
2025-01-09 01:22:58 +0800

    Update 99custom.sh

--- files/etc/uci-defaults/99custom.sh
+++ files/etc/uci-defaults/99custom.sh
@@ -11,23 +11,9 @@ uci add dhcp domain
 uci set "dhcp.@domain[-1].name=time.android.com"
 uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
-
-# 根据网卡数量配置网络
-count=0
-for iface in /sys/class/net/*; do
-  iface_name=$(basename "$iface")
-  # 检查是否为物理网卡（排除回环设备和无线设备）
-  if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
-    count=$((count + 1))
-  fi
-done
-
 # 网络设置
-if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
-elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.2.1'
-fi
+uci set network.lan.ipaddr='192.168.2.1'
+
 uci set network.wan.device='eth0'
 uci set network.wan.proto='pppoe'
 uci set network.wan.username='02700156594'
